b40f038b444ec281f01f9b5046d32e2a2770f957
Spencer <>
2021-04-29 19:31:18 -0500

    Expaned home page

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,3 +1,8 @@
+<script>
+  import MaskRaffle from "../components/embeds/MaskRaffle.svelte";
+  import { Link } from "svelte-routing";
+</script>
+
 <main>
   <br />
   <figure>
@@ -6,7 +11,7 @@
       alt="The dumpster fire boys with Justin Crichfield"
     />
     <figcaption>
-      The Dumpster Fire boys with Justin Crichfield (Middle)
+      The Dumpster Fire boys with Justin Crichfield (middle).
     </figcaption>
   </figure>
 
@@ -15,4 +20,32 @@
     wide array of content ranging from podcasts to music and everything in
     between. Thanks for reaching out!
   </p>
+  <br />
+
+  <p>
+    Dumpster Fire was founded January, 2020. Since then, it has grown to become
+    a well-established brand internationally. The current roster includes:
+  </p>
+  <ul>
+    <li>Samuel Schoonover</li>
+    <li>Spencer Solberg</li>
+    <li>Spencer Sveen</li>
+    <li>River Rommel</li>
+  </ul>
+  <figure>
+    <img
+      src="https://file.coffee/u/tyU0NAprIw.jpeg"
+      alt="Three of the Dumpster Fire boys sitting down."
+    />
+    <figcaption>
+      Three members of Dumpster Fire while they were on tour. Unfortanately,
+      Spencer Sveen was hit by a bus and was not in this photo.
+    </figcaption>
+  </figure>
+  <p>
+    Some notable projects from Dumpster Fire include <Link to="/dumpcast"
+      >Dumpcast</Link
+    > and the Dumpster Fire Mask Raffle:
+  </p>
+  <MaskRaffle />
 </main>
