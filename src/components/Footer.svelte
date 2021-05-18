a983653b4f749d3c36d99b124254292dd0172866
Spencer <>
2021-05-17 19:42:27 -0500

    Flexbox footer, fixed centering

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -13,109 +13,100 @@
 </script>
 
 <footer>
-  <ul class="social-media-list">
-    <li>
+  <div class="container">
+  
+    
       <a
         href="https://www.instagram.com/dumpsterfiremusic"
         class="logo-instagram"
         title="Instagram"
       >
-        <ion-icon name="logo-instagram" title="Instagram" />
+        <ion-icon size="large" name="logo-instagram" title="Instagram" />
       </a>
-    </li>
-    <li>
+    
+    
       <a
         href="https://www.youtube.com/channel/UCUvO8Nw5ijE2XVi110XOw7Q"
         class="logo-youtube"
         title="YouTube"
       >
-        <ion-icon name="logo-youtube" />
+        <ion-icon size="large" name="logo-youtube" />
       </a>
-    </li>
-    <li>
+    
+    
       <a
         href="https://discord.gg/A5sdKJt6Ah"
         class="logo-discord"
         title="Discord"
       >
-        <ion-icon src="images/discord.svg" />
+        <ion-icon size="large" src="images/discord.svg" />
       </a>
-    </li>
-    <li>
+    
+    
       <a
         href="https://twitter.com/dumpfiremusic"
         class="logo-twitter"
         title="Twitter"
       >
-        <ion-icon name="logo-twitter" />
+        <ion-icon size="large" name="logo-twitter" />
       </a>
-    </li>
-    <li>
+    
+    
       <a
         href="https://www.tiktok.com/@dumpsterfiremusic"
         class="logo-tiktok"
         title="TikTok"
       >
-        <ion-icon name="logo-tiktok" />
+        <ion-icon size="large" name="logo-tiktok" />
       </a>
-    </li>
-    <li>
+    
+    
       <a
         href="https://snapchat.com/add/dumpfiremusic"
         class="logo-snap"
         title="Snapchat"
       >
-        <ion-icon name="logo-snapchat" />
+        <ion-icon size="large" name="logo-snapchat" />
       </a>
-    </li>
-    <li>
+    
+    
       <a
         href="https://www.reddit.com/r/dumpsterfiremusic"
         class="logo-reddit"
         title="Reddit"
       >
-        <ion-icon name="logo-reddit" />
+        <ion-icon size="large" name="logo-reddit" />
       </a>
-    </li>
-    <li>
+    
+    
       <a
         href="https://twitch.tv/dumpsterfirestreams"
         class="logo-twitch"
         title="Twitch"
       >
-        <ion-icon name="logo-twitch" />
+        <ion-icon size="large" name="logo-twitch" />
       </a>
-    </li>
-    <li>
+    
+    
       <a
         href="https://honk.me/dumpsterfire"
         class="logo-honk"
         title="Logo Honk"
       >
-        <ion-icon src="images/honk.svg" />
+        <ion-icon src="images/honk.svg" size="large"/>
       </a>
-    </li>
-    <li>
+    
+    
       <a href="mailto:spencersolberg@dumpster.fr" class="logo-mail">
-        <ion-icon name="mail-outline" />
+        <ion-icon size="large" name="mail-outline" />
       </a>
-    </li>
-  </ul>
+</div>
   Dumpster Fire © 2021
 </footer>
 
 <style>
-  .social-media-list {
-    list-style: none;
-  }
 
-  .social-media-list li {
-    display: inline-block;
-    margin-right: 1em;
-    font-size: 180%;
-  }
-
-  .social-media-list li a {
+  .container a {
     color: #888;
     transition: color 0.5s;
   }
@@ -160,5 +151,17 @@
     color: #ead8b7;
   }
 
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 
+  .container ul {
+    padding: 0;
+  }
+
+.container a {
+  padding: 1em;
+}
 </style>
