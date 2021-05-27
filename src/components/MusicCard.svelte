262ae6e04de5b189a0c41c827e249c6b0367249e
Spencer <>
2021-05-27 15:50:06 -0500

    Preliminary card comp, added RO embed

---
+++ src/components/MusicCard.svelte
@@ -0,0 +1,65 @@
+<script>
+    export let album;
+</script>
+
+<article class="release">
+    
+    <div class="info">
+        <h2 class="title"><a href={album.url}>{album.title}</a> <ion-icon
+            class="share-button"
+            name="share-outline"
+            size="large"
+            on:click={() => {
+              navigator.share({
+                title: album.title + " - Dumpster Fire",
+                url: album.url,
+              });
+            }}
+          /></h2>
+          <div class="artwork">
+            <a href={album.url}><img src={album.artwork.src} alt={album.artwork.alt} /></a>
+        </div>
+        {#each album.paragraphs as paragraph}
+            <p class="paragraph">{paragraph}</p>
+        {/each}
+        
+    </div>
+    <nav><a href={album.url}><i class="fas fa-headphones"></i> Listen now</a></nav>
+</article>
+
+<style>
+    .release,
+    .artwork {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .artwork {
+        flex: 3 1 30ch;
+    }
+    .release {
+        background: linear-gradient(238.72deg, #FFE5C7 0%, #00C288 100%), radial-gradient(90.88% 100% at 50% 0%, #439246 0%, #183766 100%), linear-gradient(121.28deg, #FFEAB6 0%, #003C2A 100%), radial-gradient(64.89% 100% at 50% 0%, #80BAFF 0%, #001E41 100%), linear-gradient(65.05deg, #6F0072 0%, #FFC700 100%);
+background-blend-mode: overlay, screen, color-burn, lighten, normal;
+        border-radius: 5px;
+    }
+    .artwork,
+    .info {
+        padding: 5%;
+    }
+
+    .info h2 {
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    .paragraph {
+        color: rgb(220, 220, 220);
+    }
+    .share-button {
+        color: rgb(33, 33, 33);
+    }
+    .title a {
+        text-decoration: none;
+    }
+</style>
--- src/routes/Music.svelte
+++ src/routes/Music.svelte
@@ -1,6 +1,64 @@
-<h2>Coming soon! <ion-icon class="share-button" name="share-outline" size="large" on:click={() => {
-    navigator.share({
-      title: "Dumpster Fire - Music",
-      url: location.href
-    });
-  }}/></h2>
+<script>
+  import MusicCard from "../components/MusicCard.svelte";
+
+  const reachingOut = {
+    title: "REACHING OUT",
+    artwork: {
+      src: "images/REACHINGOUT.webp",
+      alt: 'The cover artwork for "REACHING OUT"',
+    },
+    paragraphs: [
+      '"REACHING OUT" is Dumpster Fire\'s debut single. This genre-defying track will reform how you percieve music. The release has been praised as one of the greatest releases of 2021, and its expected to net Dumpster Fire numerous presitious awards.',
+      "The song prominently features the four members of Dumpster Fire, as well as friends and special guest stars.",
+    ],
+    url: "https://link.dumpster.fr/reachingout"
+  };
+</script>
+
+<main>
+  <!-- <h2>
+    Coming soon! <ion-icon
+      class="share-button"
+      name="share-outline"
+      size="large"
+      on:click={() => {
+        navigator.share({
+          title: "Dumpster Fire - Music",
+          url: location.href,
+        });
+      }}
+    />
+  </h2> -->
+<br>
+  <!-- <MusicCard album={reachingOut} /> -->
+
+  <iframe width="100%" height="150" src="https://odesli.co/embed/?url=https%3A%2F%2Falbum.link%2Fjzqc9ncg3p56t&theme=dark" frameborder="0" allowfullscreen sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox" allow="clipboard-read; clipboard-write"></iframe>
+</main>
+
+<style>
+  /* .flex-span {
+    display:flex;
+    flex-wrap: wrap;
+  }
+  .reaching-out-container {
+    display: flex;
+    flex-direction: column;
+    border-radius: 5px;
+    background-color: #b02a44;
+    justify-content: space-around;
+  } */
+
+  /* .reaching-out-container h2 {
+    color: rgb(220, 220, 220);
+    margin-left: auto;
+    margin-right: auto;
+  }
+  #artwork {
+    width: 50%;
+    border-radius: 5px;
+  }
+  .reaching-out-container * {
+    padding-left: 5%;
+    padding-right: 5%;
+  } */
+</style>
